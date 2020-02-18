dd6f0195c65df4c75a8de8a9afebe12d247dd519
Bowen Zhang <zbw14@outlook.com>
2020-02-18 22:49:10 +0800

    Manage to Build a Release Version & Adjust the Finger Default State

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using System.IO;
 
 public class InputHandler : MonoBehaviour
 {
     public Camera cameraToShot = null;
+    public bool sameSizeWithWindow = false;
+    public Text debugText = null;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +23,10 @@ public class InputHandler : MonoBehaviour
         {
             SaveImage();
         }
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitApplication();
+        }
     }
 
     /// <summary>
@@ -27,13 +36,23 @@ public class InputHandler : MonoBehaviour
     {
         if(cameraToShot != null)
         {
-            Texture2D screenShot = CaptureScreen(cameraToShot);
+            Texture2D screenShot = CaptureScreen(cameraToShot, sameSizeWithWindow);
+            // debugText.text = Screen.width.ToString() + " " + Screen.height.ToString();
 
             // Save to the file
             byte[] bytes = screenShot.EncodeToPNG();
-            string filename = Application.dataPath + "/Screenshots/ScreenShot.png";
+            string foldername = Application.dataPath + "/Screenshots";
+            string filename = foldername + "/ScreenShot.png";
+
+            // Check whether the folder exists
+            if (!Directory.Exists(foldername))
+            {
+                Directory.CreateDirectory(foldername);
+            }
+
             System.IO.File.WriteAllBytes(filename, bytes);
             // Debug.Log("Saved in " + filename);
+            // debugText.text = "Saved in " + filename;
         }
     }
 
@@ -42,24 +61,35 @@ public class InputHandler : MonoBehaviour
     /// </summary>
     /// <param name="cam"></param>
     /// <returns></returns>
-    Texture2D CaptureScreen(Camera cam)
+    Texture2D CaptureScreen(Camera cam, bool isFullSize)
     {
         // Create a rendering texture
         RenderTexture rt = new RenderTexture(Screen.width, Screen.height, 0);
 
         // Assign the rendering texture to the camera
         cam.targetTexture = rt;
-        cam.Render();
+        if (isFullSize)
+        {
+            Rect oldRect = cam.rect;
+            cam.rect = new Rect(0, 0, 1, 1);
+            cam.Render();
+            cam.rect = oldRect;
+        }
+        else
+        {
+            cam.Render();
+        }
         RenderTexture.active = rt;
 
         // Create a texture 2D to store the image
-        int imageWidth = (int)(Screen.width * cam.rect.width);
-        int imageHeight = (int)(Screen.height * cam.rect.height);
+        int imageWidth = isFullSize? Screen.width : (int)(Screen.width * cam.rect.width);
+        int imageHeight = isFullSize? Screen.height : (int)(Screen.height * cam.rect.height);
         Texture2D screenShot = new Texture2D(imageWidth, imageHeight, TextureFormat.RGB24, false);
 
         // Read pixels from rendering texture (rather than the screen) to texture 2D
-        int imagePosX = (int)(Screen.width * cam.rect.x);
-        int imagePosY = (int)(Screen.height - (Screen.height * cam.rect.y + imageHeight));
+        // Note. the source coordinate is the image coordinate ((0,0) is in top-right) rather than the pixel coordinate ((0,0) is in the bottom-left)
+        int imagePosX = isFullSize? 0: (int)(Screen.width * cam.rect.x);
+        int imagePosY = isFullSize? 0: (int)(Screen.height - (Screen.height * cam.rect.y + imageHeight));
         Rect rect = new Rect(imagePosX, imagePosY, imageWidth, imageHeight);
         screenShot.ReadPixels(rect, 0, 0);
         screenShot.Apply();
@@ -70,5 +100,14 @@ public class InputHandler : MonoBehaviour
         Destroy(rt);
 
         return screenShot;
+    }
+
+    void ExitApplication()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
