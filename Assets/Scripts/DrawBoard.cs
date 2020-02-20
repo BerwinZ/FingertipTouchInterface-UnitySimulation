fa718eef0476a93b01de7b4ce5a31e781fa66767
Bowen Zhang <zbw14@outlook.com>
2020-02-21 00:40:03 +0800

    Add Touch Position Tracking

---
+++ Assets/Scripts/DrawBoard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawBoard : MonoBehaviour
+{
+    RectTransform point;
+    TouchDetection thumb;
+    float scaler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        point = transform.GetChild(0).GetComponent<RectTransform>();
+        thumb = GameObject.Find("thumb_tip").GetComponent<TouchDetection>();
+        scaler = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        point.localPosition = new Vector3(thumb.touchPosition.x * scaler, thumb.touchPosition.y * scaler, 0);
+    }
+}
--- Assets/Scripts/TouchDetection.cs
+++ Assets/Scripts/TouchDetection.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class TouchDetection : MonoBehaviour
 {
+    // The type of this finger
     public JointType.Finger fingertipType = JointType.Finger.thumb;
+
+    // Type of the finger that is touched with
     JointType.Finger detectedFingerType;
 
+    // Whether this finger is touched by another
     bool touched;
+
+    public Transform tipSpecial { get; private set; }
+
+    // The touch position of thumb relative to the index coordinate. 
+    // For index finger, it keeps (0, 0)
+    public Vector2 touchPosition { get; private set; }
 
     // Start is called before the first frame update
     void Start()
@@ -15,12 +25,16 @@ public class TouchDetection : MonoBehaviour
         detectedFingerType = (JointType.Finger)(((int)fingertipType + 1) % 2);
 
         touched = false;
+
+        tipSpecial = transform.parent.GetChild(1);
+
+        touchPosition = new Vector2(0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(fingertipType.ToString() + " " + touched);
+        // Debug.Log(fingertipType.ToString() + " Touched: " + touched);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -45,5 +59,13 @@ public class TouchDetection : MonoBehaviour
             return;
 
         touched = isTouch;
+
+        if(touched && fingertipType == JointType.Finger.thumb)
+        {
+            tipSpecial.parent = otherScript.tipSpecial;
+            touchPosition = new Vector2(-tipSpecial.localPosition.x, tipSpecial.localPosition.y);
+            tipSpecial.parent = transform.parent;
+            // Debug.Log(touchPosition);
+        }
     }
 }
